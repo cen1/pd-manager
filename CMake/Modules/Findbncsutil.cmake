5aa783a30f9f7023b0fa9ccfe448ec7f2840fa04
cen1 <cen.is.imba@gmail.com>
2025-04-19 14:13:14 +0200

    proper target support in findbncsutil module

---
+++ CMake/Modules/Findbncsutil.cmake
@@ -0,0 +1,32 @@
+# - Try to find bncsutil
+# Once done, this will define:
+#   bncsutil_FOUND - System has bncsutil
+#   bncsutil::bncsutil - Imported target
+
+find_path(bncsutil_INCLUDE_DIR
+        NAMES bncsutil/bncsutil.h
+        PATHS ${CMAKE_SOURCE_DIR}/depends/bncsutil/include
+)
+
+find_library(bncsutil_LIBRARY
+        NAMES bncsutil libbncsutil
+        PATHS ${CMAKE_SOURCE_DIR}/depends/bncsutil
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(bncsutil
+        REQUIRED_VARS bncsutil_INCLUDE_DIR bncsutil_LIBRARY
+)
+
+mark_as_advanced(bncsutil_INCLUDE_DIR bncsutil_LIBRARY)
+
+if(bncsutil_FOUND AND NOT TARGET bncsutil::bncsutil)
+    add_library(bncsutil::bncsutil UNKNOWN IMPORTED)
+    set_target_properties(bncsutil::bncsutil PROPERTIES
+            IMPORTED_LOCATION "${bncsutil_LIBRARY}"
+            INTERFACE_INCLUDE_DIRECTORIES "${bncsutil_INCLUDE_DIR}"
+    )
+endif()
+
+MESSAGE(STATUS "bncsutil include: " ${BNCSUTIL_INCLUDE_DIR})
+MESSAGE(STATUS "bncsutil lib: " ${BNCSUTIL_LIBRARY})
